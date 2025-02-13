7b60cee1cecce9d2fa70ecb26c7ecc9552343254
Leonardo Dettmann Krüger <leokruger2012@gmail.com>
2025-02-13 16:52:27 -0300

    Initial commit

---
+++ src/database.ts
@@ -0,0 +1,21 @@
+
+
+import { knex as setupKnex, Knex } from "knex"
+import { env } from "./env"
+
+
+
+export const config: Knex.Config = {
+    client: 'sqlite',
+    connection: {
+        database: "",
+        filename: env.DATABASE_URL
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: "ts",
+        directory: "./database/migrations"
+    }
+}
+
+export const knex = setupKnex(config)
